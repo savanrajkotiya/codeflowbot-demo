1526ef3087abe2e1a7e9e90b0910566bc84a128e
savanrajkotiya <savanrajkotiya@gmail.com>
2022-10-21 13:25:52 +0530

    changes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,7 +6,7 @@ export default component$(() => {
   return (
     <div>
       <h1>
-        Welcome to Qwik <span class="lightning">⚡️</span>
+        Welcome to Qwik! <span class="lightning">⚡️</span>
       </h1>
 
       <ul>
